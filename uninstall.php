710be044e93fa1b15555b05a87e7221963f3eee8
Luffy <52o@qq52o.cn>
2024-12-30 11:53:40 +0800

    fix

--- uninstall.php
+++ uninstall.php
@@ -1,8 +1,10 @@
 <?php
 
-if (!defined('WP_UNINSTALL_PLUGIN')) exit();
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    exit();
+}
 
-$obs_options = get_option('obs_options', true);
+$obs_options = get_option('obs_options', obs_get_default_options());
 $upload_url_path = get_option('upload_url_path');
 $obs_upload_url_path = esc_attr($obs_options['upload_url_path']);
 
